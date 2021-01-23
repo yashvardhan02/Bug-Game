fe3c35169b856924ba6fef9b13c40ad761e7e406
yashvardhan02 <72331528+yashvardhan02@users.noreply.github.com>
2021-01-23 11:51:59 +0530

    Add files via upload

--- p5.play-boilerplate-master (5)/p5.play-boilerplate-master/food.js
+++ p5.play-boilerplate-master (5)/p5.play-boilerplate-master/food.js
@@ -7,4 +7,19 @@ var randy = random(20,380);
 foodparticle1 = createSprite(randx,randy,10,10)
 foodparticle1.shapeColor = "red"
 
+
+
+computerbug.x = foodparticle1.x
+
+if( foodparticle1.y < 200){
+   
+   computerbug.velocityY = -1
+   computerbug.rotation = -90
+}
+
+if(foodparticle1.y > 200){
+
+  computerbug.velocityY = 1
+  computerbug.rotation = 90
+}
 }
--- p5.play-boilerplate-master (5)/p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master (5)/p5.play-boilerplate-master/sketch.js
@@ -7,7 +7,12 @@ var wall4
 var enemybugimg
 var playerbugimg
 var foodparticle1;
+var arrowup
+var arrowdown
+var arrowleft
+var arrowright
 
+var arrowimg 
 
 var gameState = "Start"
 
@@ -17,7 +22,7 @@ function preload(){
 
   enemybugimg = loadImage('enemybug.png')
   playerbugimg = loadImage('playerbug.png')
-
+  arrowimg = loadImage('Arrow.png')
 
 }
 
@@ -26,14 +31,34 @@ function setup() {
   playerbug = createSprite(100, 200, 10, 10);
   computerbug = createSprite(400,200,10,10);
 
+  arrowup = createSprite(420,300,10,10)
+  arrowleft = createSprite(380,330,10,10)
+  arrowright = createSprite(460,330,10,10)
+  arrowdown = createSprite(420,365,10,10)
+
   playerbug.addImage(playerbugimg)
   computerbug.addImage(enemybugimg)
+
+  arrowup.addImage(arrowimg)
+  arrowleft.addImage(arrowimg)
+  arrowright.addImage(arrowimg)
+  arrowdown.addImage(arrowimg)
+
 
   playerbug.scale = 0.11
   computerbug.scale = 0.11
 
+  arrowup.scale = 0.09
+  arrowleft.scale = 0.09
+  arrowright.scale = 0.09
+  arrowdown.scale = 0.09
+
   playerbug.rotation = -90
   computerbug.rotation = -90
+
+  arrowup.rotation = 90;
+  arrowright.rotation = 180
+  arrowdown.rotation = 270
 
   wall1 = createSprite(0,395,1000,10);
   wall2 = createSprite(0,5,1000,10);
@@ -69,7 +94,7 @@ function draw() {
 
   if(gameState == "play"){
      
-    if(keyDown (UP_ARROW)){
+    if(keyDown (UP_ARROW) || mousePressedOver(arrowup)){
       playerbug.velocityY = -3
       playerbug.velocityX = 0
 
@@ -77,28 +102,30 @@ function draw() {
     }
     
     
-    if(keyDown (DOWN_ARROW)){
+    if(keyDown (DOWN_ARROW) || mousePressedOver(arrowdown)){
       playerbug.velocityY = 3
       playerbug.velocityX = 0
 
       playerbug.rotation = 90;
     }
     
-    if(keyDown (LEFT_ARROW)){
+    if(keyDown (LEFT_ARROW) || mousePressedOver(arrowleft)){
       playerbug.velocityX = -3
       playerbug.velocityY = 0
 
       playerbug.rotation = 180
     }
     
-    if(keyDown (RIGHT_ARROW)){
+    if(keyDown (RIGHT_ARROW) || mousePressedOver(arrowright)){
       playerbug.velocityX = 3
       playerbug.velocityY = 0
 
       playerbug.rotation = 0
     }
     
- 
+    
+
+     
 
     if(playerbug.isTouching(foodparticle1)){
       foodparticle1.destroy()
@@ -113,8 +140,14 @@ function draw() {
 
     }
 
-    
+  
      
+
+    computerbug.bounceOff(wall1);
+    computerbug.bounceOff(wall2);
+    computerbug.bounceOff(wall3);
+    computerbug.bounceOff(wall4);
+
     drawSprites();    
     
     stroke("red")
